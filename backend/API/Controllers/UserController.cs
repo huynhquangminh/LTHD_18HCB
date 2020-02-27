5e5425028a02933161227bbd3a29033f35383c04
John <flippy2016@gmail.com>
2020-02-27 22:50:00 +0700

    Generate Jwt Token after login success

--- backend/API/Controllers/UserController.cs
+++ backend/API/Controllers/UserController.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using BusinessLogic.Service.Interface;
 using BusinessObject;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace API.Controllers
 {
@@ -13,15 +18,17 @@ namespace API.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private IConfiguration _config;
         private readonly IUserService _userService;
 
-        public UserController(IUserService userService)
+        public UserController(IConfiguration config, IUserService userService)
         {
+            _config = config;
             _userService = userService;
         }
 
         /// <summary>
-        /// Get all employees
+        /// Get all users
         /// </summary>
         /// <returns></returns>
         [HttpGet]
@@ -31,6 +38,91 @@ namespace API.Controllers
         {
             var result = await _userService.GetAllUsers();
             return Ok(result);
+        }
+
+        /// <summary>
+        /// Get User by user name and password
+        /// </summary>
+        /// <param name="userName"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("GetUserByUserNamePassword")]
+        public async Task<ActionResult<UserBO>> GetUserByUserNamePassword(string userName, string password)
+        {
+            if (_userService.GetUserByUserNamePassword(userName, password).Result == null)
+            {
+                return NotFound();
+            }
+
+            return await _userService.GetUserByUserNamePassword(userName,password);
+        }
+
+        /// <summary>
+        /// Add new user
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Produces("application/json")]
+        [Route("AddUser")]
+        public async Task<ActionResult<int>> AddUser(UserBO user)
+        {
+            return await _userService.AddUser(user);
+        }
+
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("Login")]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            UserBO login = new UserBO();
+            login.UserName = username;
+            login.Password = password;
+
+            IActionResult response = Unauthorized();
+
+            var user = AuthenticateUser(login);
+
+            if (user != null)
+            {
+                var stringToken = GenerateJSONWebToken(user);
+                response = Ok(new { token = stringToken });
+            }
+
+            return response;
+        }
+
+        private UserBO AuthenticateUser(UserBO login)
+        {
+            UserBO user = null;
+            user = _userService.GetUserByUserNamePassword(login.UserName, login.Password).Result;
+
+            return user;
+        }
+
+        private string GenerateJSONWebToken(UserBO userInfo)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames.Sub, userInfo.UserName),
+                new Claim(Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames.Email, userInfo.EmailAddress),
+                new Claim(Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials);
+
+            var encodeToken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodeToken;
         }
     }
 }
