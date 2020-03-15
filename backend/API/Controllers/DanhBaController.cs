a244c371e48090f5c86efd582d7fc36fc11cbdad
John <flippy2016@gmail.com>
2020-03-15 11:54:43 +0700

    change configuration declare

--- backend/API/Controllers/DanhBaController.cs
+++ backend/API/Controllers/DanhBaController.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using AutoMapper.Configuration;
 using BusinessLogic.Service.Interface;
 using BusinessObject;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace API.Controllers
 {
