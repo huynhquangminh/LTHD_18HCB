73433050511dd642dbb6d695a63e2a4ef331d499
John <flippy2016@gmail.com>
2020-05-02 20:42:49 +0700

    Create api GetThongTinTaiKhoan on NganHangLienKetController

--- backend/API/Controllers/NganHangLienKetController.cs
+++ backend/API/Controllers/NganHangLienKetController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Services;
 using BusinessLogic.Service.Interface;
 using BusinessObject;
 using Microsoft.AspNetCore.Authorization;
@@ -20,16 +21,19 @@ namespace API.Controllers
     {
         private IConfiguration _config;
         private readonly INganHangLienKetService _nganHangLienKetService;
+        private readonly IUserService _userService;
 
         /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="config"></param>
         /// <param name="nganHangLienKetService"></param>
-        public NganHangLienKetController(IConfiguration config, INganHangLienKetService nganHangLienKetService)
+        /// <param name="userService"></param>
+        public NganHangLienKetController(IConfiguration config, INganHangLienKetService nganHangLienKetService, IUserService userService)
         {
             _config = config;
             _nganHangLienKetService = nganHangLienKetService;
+            _userService = userService;
         }
 
         /// <summary>
@@ -44,6 +48,44 @@ namespace API.Controllers
         {
             var result = await _nganHangLienKetService.GetDanhSach();
             return Ok(result);
+        }
+
+        /// <summary>
+        /// Lấy thông tin tài khoản theo số tài khoản
+        /// </summary>
+        /// <param name="soTaiKhoan"></param>
+        /// <param name="timer"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Produces("application/json")]
+        [Route("GetThongTinTaiKhoan")]
+        public async Task<IActionResult> GetThongTinTaiKhoan(string soTaiKhoan, string timer)
+        {
+            var secretKey = "nhom9";
+            var hashString = $"{soTaiKhoan}{timer}{secretKey}";
+            var result = CheckHash(soTaiKhoan, timer, secretKey);
+
+            if (result)
+            {
+                var user = _userService.GetThongTinTaiKhoanBySoTaiKhoan(soTaiKhoan);
+                return Ok(new { user.Result, status = true });
+            }
+
+            return Ok(new {mesError = "request false", status = false });
+        }
+
+        private bool CheckHash(string soTaiKhoan, string time, string secretKey)
+        {
+            var result = false;
+            var checkString = $"{soTaiKhoan}{time}{secretKey}";
+
+            var checkHash = BCryptService.HashPassword(checkString);
+
+            if (BCryptService.CheckPassword(checkString, checkHash))
+            {
+                result = true;
+            }
+            return result;
         }
     }
 }
